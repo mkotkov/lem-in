33ccd2e81fb2a42c45f286b717c1b201f992823d
mkotkov <maximkotkov@gmail.com>
2023-11-10 19:30:54 +0200

    finale_fimale

--- internal/result.go
+++ internal/result.go
@@ -9,107 +9,61 @@ import (
 )
 
 func RunProgram(path string, showContent bool) {
-	out := os.Stdout
-	if showContent {
-		err := WriteFileContent(out, path)
-		if err != nil {
-			fmt.Printf("ERROR: %v\n", err)
-			os.Exit(1)
-		}
-	}
-
-	err := WriteResult(out, path)
+	err := WriteResultByPath(os.Stdout, path, showContent)
 	if err != nil {
 		fmt.Printf("ERROR: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func WriteFileContent(w io.Writer, path string) error {
+func WriteResultByPath(w io.Writer, path string, writeContent bool) error {
 	file, err := os.Open(path)
 	if err != nil {
-		return fmt.Errorf("WriteFileContent: %w", err)
+		return fmt.Errorf("WriteResultByPath: %w", err)
 	}
 	defer file.Close()
 
 	if fi, err := file.Stat(); err != nil || fi.IsDir() {
-		return fmt.Errorf("WriteFileContent: invalid file: %v", path)
-	}
-
-	_, err = io.Copy(w, file)
-	if err != nil {
-		return fmt.Errorf("WriteFileContent: %w", err)
-	}
-	return nil
-}
-
-func WriteResult(w io.Writer, path string) error {
-	file, err := os.Open(path)
-	if err != nil {
-		return fmt.Errorf("WriteResult: %w", err)
-	}
-	defer file.Close()
-
-	if fi, err := file.Stat(); err != nil || fi.IsDir() {
-		return fmt.Errorf("WriteResult: invalid file: %v", path)
+		return fmt.Errorf("WriteResultByPath: invalid file: %v", path)
 	}
 
 	scanner := bufio.NewScanner(file)
-	terrain := CreateAntFarm()
-	var errAnt error
-	for scanner.Scan() {
-		errAnt = terrain.ReadDataFromLine(scanner.Text())
-		if errAnt != nil {
-			return fmt.Errorf("WriteResult: %w", errInvalidDataFormat(errAnt))
-		}
-	}
-	errAnt = terrain.ValidateByFieldInfo()
-	if errAnt != nil {
-		return fmt.Errorf("WriteResult: %w", errInvalidDataFormat(errAnt))
-	}
-	errAnt = terrain.Match()
-	if errAnt != nil {
-		return fmt.Errorf("WriteResult: %w", errPaths(errAnt))
+	result, err := GetResult(scanner)
+	if err != nil {
+		return fmt.Errorf("WriteResultByPath: %w", err)
 	}
 
-	sort.Slice(terrain.Result.Paths, func(i, j int) bool { return terrain.Result.Paths[i].Len < terrain.Result.Paths[j].Len })
-	steps, antsForEachPath := calcSteps(terrain.Result.AntsCount, terrain.Result.Paths)
-
-	if steps == 1 {
-		roomName := terrain.Result.Paths[0].Front.Room.Name
-		for ant := 1; ant <= antsForEachPath[0]; ant++ {
-			fmt.Fprintf(w, "L%d-%s ", ant, roomName)
+	if writeContent {
+		file.Seek(0, io.SeekStart)
+		_, err = io.Copy(w, file)
+		if err != nil {
+			return fmt.Errorf("WriteResultByPath: %w", err)
 		}
-		fmt.Fprintln(w)
-	} else {
-		paths := pathsOfListToSlice(terrain.Result.Paths)
-		a, b := &AntQueue{}, &AntQueue{}
-		antNum := 1
-		for i := 1; i <= steps; i++ {
-			cur := a.Dequeue()
-			for cur != nil {
-				w.Write([]byte(fmt.Sprintf("L%d-%s ", cur.Num, paths[cur.Path][cur.Pos].Name)))
-				cur.Pos++
-				if cur.Pos < len(paths[cur.Path]) {
-					b.EnqueueAnt(cur)
-				}
-				cur = a.Dequeue()
-			}
-			for j, v := range antsForEachPath {
-				if v > 0 {
-					w.Write([]byte(fmt.Sprintf("L%d-%s ", antNum, paths[j][0].Name)))
-					antsForEachPath[j]--
-					b.Enqueue(antNum, j, 1)
-					antNum++
-				}
-			}
-			t := a
-			a = b
-			b = t
-			fmt.Fprintln(w)
-		}
+		fmt.Fprint(w, "\n\n# result\n")
 	}
+	result.WriteResult(w)
+
 	return nil
+}
+
+func GetResult(scanner *bufio.Scanner) (*Result, error) {
+	terrain := CreateAntFarm()
+	var err error
+	for scanner.Scan() {
+		err = terrain.ReadDataFromLine(scanner.Text())
+		if err != nil {
+			return nil, errInvalidDataFormat(err)
+		}
+	}
+	err = terrain.ValidateByFieldInfo()
+	if err != nil {
+		return nil, errInvalidDataFormat(err)
+	}
+	err = terrain.Match()
+	if err != nil {
+		return nil, errPaths(err)
+	}
+	return terrain.Result, nil
 }
 
 func pathsOfListToSlice(paths []*List) [][]*Room {
@@ -123,4 +77,45 @@ func pathsOfListToSlice(paths []*List) [][]*Room {
 		}
 	}
 	return result
+}
+
+func (r *Result) WriteResult(w io.Writer) {
+	sort.Slice(r.Paths, func(i, j int) bool { return r.Paths[i].Len < r.Paths[j].Len })
+	steps, antsForEachPath := calcSteps(r.AntsCount, r.Paths)
+	if steps == 1 {
+		roomName := r.Paths[0].Front.Room.Name
+		for ant := 1; ant <= antsForEachPath[0]; ant++ {
+			fmt.Fprintf(w, "L%d-%s ", ant, roomName)
+		}
+		fmt.Fprintln(w)
+	} else {
+		paths := pathsOfListToSlice(r.Paths)
+		a, b := &AntQueue{}, &AntQueue{}
+		antNum := 1
+		for i := 1; i <= steps; i++ {
+			cur := a.Dequeue()
+			for cur != nil {
+
+				w.Write([]byte(fmt.Sprintf("L%d-%s ", cur.Num, paths[cur.Path][cur.Pos].Name)))
+				cur.Pos++
+				if cur.Pos < len(paths[cur.Path]) {
+					b.EnqueueAnt(cur)
+				}
+				cur = a.Dequeue()
+			}
+			for j, v := range antsForEachPath {
+				if v > 0 {
+
+					w.Write([]byte(fmt.Sprintf("L%d-%s ", antNum, paths[j][0].Name)))
+					antsForEachPath[j]--
+					b.Enqueue(antNum, j, 1)
+					antNum++
+				}
+			}
+			t := a
+			a = b
+			b = t
+			fmt.Fprintln(w)
+		}
+	}
 }
